a0626d4a8d5f44efae3c4a21a3b40e3694ccc66a
Amaan Qureshi <amaanq12@gmail.com>
2023-07-13 13:07:38 -0400

    chore: update scanner

--- src/scanner.c
+++ src/scanner.c
@@ -6,8 +6,8 @@
 // under MIT
 
 enum TokenType {
-  MULTILINE_COMMENT,
-  /* PREPROC_BLOCK, */
+    MULTILINE_COMMENT,
+    /* PREPROC_BLOCK, */
 };
 
 void *tree_sitter_tablegen_external_scanner_create() { return NULL; }
@@ -18,7 +18,7 @@ void tree_sitter_tablegen_external_scanner_reset(void *payload) {}
 
 unsigned tree_sitter_tablegen_external_scanner_serialize(void *payload,
                                                          char *buffer) {
-  return 0;
+    return 0;
 }
 
 void tree_sitter_tablegen_external_scanner_deserialize(void *payload,
@@ -27,53 +27,55 @@ void tree_sitter_tablegen_external_scanner_deserialize(void *payload,
 
 bool tree_sitter_tablegen_external_scanner_scan(void *payload, TSLexer *lexer,
                                                 const bool *valid_symbols) {
-  if (valid_symbols[MULTILINE_COMMENT]) {
-    while (iswspace(lexer->lookahead))
-      lexer->advance(lexer, true);
-
-    if (lexer->lookahead != '/')
-      return false;
-    lexer->advance(lexer, false);
-    if (lexer->lookahead != '*')
-      return false;
-    lexer->advance(lexer, false);
-
-    bool after_star = false;
-    unsigned nesting_depth = 1;
-    for (;;) {
-
-      switch (lexer->lookahead) {
-      case '\0':
-        return false;
-      case '*':
-        lexer->advance(lexer, false);
-        after_star = true;
-        break;
-      case '/':
-        if (after_star) {
-          lexer->advance(lexer, false);
-          after_star = false;
-          nesting_depth--;
-          if (nesting_depth == 0) {
-            lexer->result_symbol = MULTILINE_COMMENT;
-            return true;
-          }
-        } else {
-          lexer->advance(lexer, false);
-          after_star = false;
-          if (lexer->lookahead == '*') {
-            nesting_depth++;
-            lexer->advance(lexer, false);
-          }
+    if (valid_symbols[MULTILINE_COMMENT]) {
+        while (iswspace(lexer->lookahead)) {
+            lexer->advance(lexer, true);
         }
-        break;
-      default:
-        lexer->advance(lexer, false);
-        after_star = false;
-        break;
-      }
-    }
-  }
 
-  return false;
+        if (lexer->lookahead != '/') {
+            return false;
+        }
+        lexer->advance(lexer, false);
+        if (lexer->lookahead != '*') {
+            return false;
+        }
+        lexer->advance(lexer, false);
+
+        bool after_star = false;
+        unsigned nesting_depth = 1;
+        for (;;) {
+            switch (lexer->lookahead) {
+                case '\0':
+                    return false;
+                case '*':
+                    lexer->advance(lexer, false);
+                    after_star = true;
+                    break;
+                case '/':
+                    if (after_star) {
+                        lexer->advance(lexer, false);
+                        after_star = false;
+                        nesting_depth--;
+                        if (nesting_depth == 0) {
+                            lexer->result_symbol = MULTILINE_COMMENT;
+                            return true;
+                        }
+                    } else {
+                        lexer->advance(lexer, false);
+                        after_star = false;
+                        if (lexer->lookahead == '*') {
+                            nesting_depth++;
+                            lexer->advance(lexer, false);
+                        }
+                    }
+                    break;
+                default:
+                    lexer->advance(lexer, false);
+                    after_star = false;
+                    break;
+            }
+        }
+    }
+
+    return false;
 }
